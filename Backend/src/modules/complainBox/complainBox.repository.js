a17eb99521390d76f8a95a46c25d5d776f643018
2020331002_Tanvir <95771942+rayhanulamint2@users.noreply.github.com>
2025-06-14 11:32:50 +0600

    complaint box can be marked as solved
    Now complaint functionality is fully working in the frontend. Complaints are shown in the frontend after fetching. And complaints can be solved from the admin panel.

--- Backend/src/modules/complainBox/complainBox.repository.js
+++ Backend/src/modules/complainBox/complainBox.repository.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose')
 const complainSchema = require('./complainBox.model')
-const Complain = new mongoose.model('Complain',complainSchema);
+const Complain = new mongoose.model('Complain', complainSchema);
 
-const newComplainCreation = async(complainData) => {
+const newComplainCreation = async (complainData) => {
     const newComplain = new Complain(complainData);
-    try{
+    try {
         await newComplain.save();
         console.log('complain created successfully')
         return newComplain;
-    } catch(error){
+    } catch (error) {
         console.log('there are an error in server end')
     }
 }
-const findComplain = async(id)=>{
-    try{
-        const complain = await Complain.findById(id)
-        return complain
+const findComplain = async () => {
+    try {
+        const complaints = await Complain.find({ solved: false });
+        return complaints;
+    } catch (error) {
+        console.log('There was an error on the server end:', error);
     }
-    catch(error){
-        console.log('there are an error in server end')
-    }
+
 }
 
-const deleteComplainByID = async(id) => {
+const deleteComplainByID = async (id) => {
     try {
         await Complain.findByIdAndDelete(id);
         console.log('Complain deleted successfully');
@@ -31,20 +31,22 @@ const deleteComplainByID = async(id) => {
     }
 }
 
-const complainUpdate = async(id, updateData) => {
+const complainUpdate = async (updateData) => {
     try {
-        console.log('Updating complain with ID:', id);
+        console.log('Updating complain with ID:', updateData.id);
         console.log('Update data:', updateData);
-        const updatedComplain = await Complain.findByIdAndUpdate(id, updateData, { new: true });
+        const updatedComplain = await Complain.findById(updateData.id);
         if (!updatedComplain) {
             throw new Error('Complain not found');
         }
         console.log('Complain updated successfully');
+        updatedComplain.solved = true;
         console.log(updatedComplain);
+        updatedComplain.save();
         return updatedComplain;
     } catch (error) {
         console.log('there are an error in server end');
         console.error(error);
     }
 }
-module.exports = {newComplainCreation, findComplain, deleteComplainByID, complainUpdate}
+module.exports = { newComplainCreation, findComplain, deleteComplainByID, complainUpdate }
